19844b283d38cf0fdaadaaf371395bf078be74de
Akira Osada <osd.akira@gmail.com>
2014-12-17 11:52:13 +0900

    cannot override lib/templates/scaffold/controller_spec

--- lib/generators/action_args/rspec/scaffold/scaffold_generator.rb
+++ lib/generators/action_args/rspec/scaffold/scaffold_generator.rb
@@ -10,8 +10,13 @@ module Rspec
       def generate_controller_spec
         return unless options[:controller_specs]
 
-        template 'action_args_controller_spec.rb',
-                 File.join('spec/controllers', controller_class_path, "#{controller_file_name}_controller_spec.rb")
+        template_file = File.join(
+          'spec/controllers',
+          controller_class_path,
+          "#{controller_file_name}_controller_spec.rb"
+        )
+
+        template 'controller_spec.rb', template_file
       end
     end
   end
